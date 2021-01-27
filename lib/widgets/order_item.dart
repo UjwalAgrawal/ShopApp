34ee280b8c536435713aecf5ee77b44a25140d86
UjwalAgrawal <uvagrawal123@gmail.com>
2021-01-27 13:40:02 +0530

    orders animation

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -17,28 +17,36 @@ class _OrderItemState extends State<OrderItem> {
   bool _isExpanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("\$${widget.order.amount}"),
-            subtitle: Text(
-              DateFormat('dd-MMM-yy hh:mm:ss').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
+      height: _isExpanded
+          ? min(widget.order.products.length * 20.0 + 110, 200.0)
+          : 95,
+      // curve: Curves.easeInOutQuint,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("\$${widget.order.amount}"),
+              subtitle: Text(
+                DateFormat('dd-MMM-yy hh:mm:ss').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _isExpanded = !_isExpanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _isExpanded = !_isExpanded;
-                });
-              },
-            ),
-          ),
-          if (_isExpanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 200),
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100.0),
+              height: _isExpanded
+                  ? min(widget.order.products.length * 20.0 + 10, 100.0)
+                  : 0,
               child: ListView(
                 children: widget.order.products
                     .map((prod) => Row(
@@ -47,19 +55,20 @@ class _OrderItemState extends State<OrderItem> {
                             Text(
                               prod.title,
                               style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
+                                  fontSize: 16, fontWeight: FontWeight.bold),
                             ),
                             Text(
                               "${prod.quantity} x \$${prod.price}",
                               style:
-                                  TextStyle(fontSize: 18, color: Colors.grey),
+                                  TextStyle(fontSize: 16, color: Colors.grey),
                             ),
                           ],
                         ))
                     .toList(),
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
